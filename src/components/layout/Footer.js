9003660a1a23a7ffad17af8b0063534be08ae0c2
zjpg <71420644+zjpg@users.noreply.github.com>
2021-03-07 02:35:23 +0700

    Initial

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Grid from 'react-bootstrap/lib/Grid'
+import Row from 'react-bootstrap/lib/Row'
+import Col from 'react-bootstrap/lib/Col'
+// import LumensRates from '../shared/LumensRates'
+
+class Footer extends React.PureComponent {
+  render() {
+    return (
+      <Grid id="footer">
+        <Row>
+          <Col md={3}>
+            {
+              // Disable rates for now, it doesn't refresh correctly: <LumensRates/>
+            }
+          </Col>
+          <Col mdOffset={7} md={2}>
+            <a href="https://github.com/chatch/stellarexplorer">
+              <img
+                src={`${process.env.PUBLIC_URL}/img/gh.svg`}
+                alt="github"
+                height={20}
+                width={20}
+              />
+              Source Code
+            </a>
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
+}
+
+export default Footer
